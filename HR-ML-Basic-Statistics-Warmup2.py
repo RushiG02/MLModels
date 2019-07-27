9e6b48ce26320621ceb5996c7eb15a2bf7420f8e
akiwelekar <akiwelekar@gmail.com>
2019-07-27 12:09:35 +0530

    Add files via upload

---
+++ HR-ML-Basic-Statistics-Warmup2.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy.stats as stats
+
+number_array =list()
+n = int(raw_input())
+number_array = list(map(int,raw_input().split()))
+print(np.mean(number_array))
+print(np.median(number_array))
+mode = stats.mode(number_array, axis = None)
+print(int(mode[0]))
+print("%.1f" %np.std(number_array))
+
+std    = round(np.std(number_array),1)
+low    = round(np.mean(number_array)-1.960*(np.std(number_array)/(n**0.5)),1)
+upp    = round(np.mean(number_array)+1.960*(np.std(number_array)/(n**0.5)),1)
+print low, upp
+
